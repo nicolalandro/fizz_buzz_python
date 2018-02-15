a85fb1dfaeb26d93f151160f436460a52ba614c2
nicolalandro <nicolaxx94@live.it>
2018-02-15 14:34:18 +0100

    strat with travis

---
+++ test/main_test.py
@@ -0,0 +1,12 @@
+import subprocess
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_main(self):
+        output = subprocess.check_output(['python', '../src/main.py'])
+        self.assertEqual("b'ciao\\n'", str(output))
+
+
+if __name__ == '__main__':
+    unittest.main()

95ca0d0eb534e02472cb74efcfdbaf4a1ca24bea
nicolalandro <nicolaxx94@live.it>
2018-02-17 14:58:31 +0100

    fizz_buzzer integrato nel main

--- src/main.py
+++ src/main.py
@@ -1,5 +1,11 @@
 import sys
 
+from src.fizz_buzzer import FizzBuzzer
+from src.number import Number
+
 if __name__ == '__main__':
+    fizz_buzzer = FizzBuzzer()
     for i in range(1, int(sys.argv[1]) + 1):
-        print(i)
+        i_number = Number(i)
+        speach_text = fizz_buzzer.speach_text_for(i_number)
+        print(speach_text)
--- test/main_test.py
+++ test/main_test.py
@@ -1,7 +1,7 @@
 import subprocess
 import unittest
 
-CORRECT_OUTPUT = "b'1\\n2\\n3\\n4\\n5\\n6\\n7\\n8\\n9\\n10\\n11\\n12\\n13\\n14\\n15\\n'"
+CORRECT_OUTPUT = "b'1\\n2\\nfizz\\n4\\n5\\nfizz\\n7\\n8\\nfizz\\n10\\n11\\nfizz\\n13\\n14\\nfizz\\n'"
 
 
 class MyTestCase(unittest.TestCase):

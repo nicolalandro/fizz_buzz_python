d9e893e8efb1deea190d87d0b28901a432d511c2
nicolalandro <nicolaxx94@live.it>
2018-02-17 14:27:08 +0100

    repete number

--- src/main.py
+++ src/main.py
@@ -1,2 +1,5 @@
+import sys
+
 if __name__ == '__main__':
-    print('ciao')
+    for i in range(1, int(sys.argv[1]) + 1):
+        print(i)
--- test/main_test.py
+++ test/main_test.py
@@ -1,11 +1,13 @@
 import subprocess
 import unittest
 
+CORRECT_OUTPUT = "b'1\\n2\\n3\\n4\\n5\\n6\\n7\\n8\\n9\\n10\\n11\\n12\\n13\\n14\\n15\\n'"
+
 
 class MyTestCase(unittest.TestCase):
     def test_main(self):
-        output = subprocess.check_output(['python', 'src/main.py'])
-        self.assertEqual("b'ciao\\n'", str(output))
+        output = subprocess.check_output(['python', 'src/main.py', '15'])
+        self.assertEqual(CORRECT_OUTPUT, str(output))
 
 
 if __name__ == '__main__':

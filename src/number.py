bdfb472fdeefd73ac7f81765ccff0de4f178b14b
nicolalandro <nicolaxx94@live.it>
2018-02-17 08:46:03 +0100

    object number

---
+++ src/number.py
@@ -0,0 +1,6 @@
+class Number:
+    def __init__(self, int_number):
+        self.value = int_number
+
+    def is_disible_for(self, n):
+        return self.value % n.value == 0
---
+++ test/number_test.py
@@ -0,0 +1,23 @@
+import unittest
+
+from src.number import Number
+
+
+class NumberTestCase(unittest.TestCase):
+    def test_create_number(self):
+        number = Number(5)
+        self.assertEqual(5, number.value)
+
+    def test_is_divisible_for(self):
+        number = Number(5)
+        divisible = number.is_disible_for(Number(5))
+        self.assertTrue(divisible)
+
+    def test_is_not_divisible_for(self):
+        number = Number(5)
+        divisible = number.is_disible_for(Number(2))
+        self.assertFalse(divisible)
+
+
+if __name__ == '__main__':
+    unittest.main()

997b87f88a62c9360299cb46ebb3f6d99697d368
nicolalandro <nicolaxx94@live.it>
2018-02-17 08:52:58 +0100

    TMTH

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='FizzBuzz',
+    version='0.1',
+    description='Python study',
+    author='Nicola Landro',
+    author_email='nicolaxx94@live.it',
+    url='https://github.com/nicolalandro/fizz_buzz_python',
+    packages=['src'],
+)
